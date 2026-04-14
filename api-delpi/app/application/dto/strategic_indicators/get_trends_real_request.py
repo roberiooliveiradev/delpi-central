4ffde29c941c16510cab777e341a03ddc863364c
roberiooliveiradev <roberio.oliveira.trabalho@gmail.com>
2026-04-14 17:39:38 -0300

    fix(strategic-indicator): typo in strategic indicators trends request department filter

--- api-delpi/app/application/dto/strategic_indicators/get_trends_real_request.py
+++ api-delpi/app/application/dto/strategic_indicators/get_trends_real_request.py
@@ -5,7 +5,7 @@ from dataclasses import dataclass
 
 @dataclass
 class GetStrategicIndicatorsTrendsRealRequest:
-    departmant_id: str | None = None
+    department_id: str | None = None
     branch: str | None = None
     start_date: str | None = None
     end_date: str | None = None
