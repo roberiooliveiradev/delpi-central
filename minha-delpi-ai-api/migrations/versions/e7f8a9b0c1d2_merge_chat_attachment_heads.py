e49965c6b0add2bad7e36aafb8369037ffa21920
roberiooliveiradev <roberio.oliveira.trabalho@gmail.com>
2026-05-13 13:46:04 -0300

    fix(chat): merge attachment migration heads

---
+++ minha-delpi-ai-api/migrations/versions/e7f8a9b0c1d2_merge_chat_attachment_heads.py
@@ -0,0 +1,19 @@
+"""merge chat attachment heads
+
+Revision ID: e7f8a9b0c1d2
+Revises: d9e5a1c4b8f2, d1e2f3a4b5c6
+Create Date: 2026-05-13 00:00:00.000000
+"""
+
+revision = "e7f8a9b0c1d2"
+down_revision = ("d9e5a1c4b8f2", "d1e2f3a4b5c6")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
